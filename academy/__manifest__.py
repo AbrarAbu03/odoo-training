04f289700466867ea3441495c8eee8fc679916cc
abrar <abrarm.appscomp@gmail.com>
2022-08-20 18:45:19 +0530

    Academy Module Added

---
+++ academy/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Academy",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['website'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'demo/data.xml',
+        'views/template_page_menu.xml',
+        'views/templates.xml',
+        'views/add_value_templates.xml',
+        'views/snippets.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'academy/static/src/scss/academy.scss',
+            'academy/static/src/js/add_value.js',
+        ],
+    },
+    'demo': [],
+    'license': 'LGPL-3',
+}
---
+++ academy/controllers/controllers.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+
+
+class Academy(http.Controller):
+
+    @http.route('/info', auth='public')
+    def index(self, **kw):
+        return "Hello, World"
+
+    # added by abrar
+    @http.route('/channel', auth='public', csrf=False, website=True)
+    def index(self, **kw):
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        # return "controller is calling"
+        # return request.render('academy.news_created',{})
+        return request.render('academy.user_details', {})
+
+    # ended by abrar
+
+    @http.route('/academy/academy/objects', auth='public', website=True)
+    def list(self, **kw):
+        return request.render('academy.listing', {
+            'root': '/academy/academy',
+            'objects': request.env['academy.academy'].search([]),
+        })
+
+    # Added by Appscomp
+    @http.route('/info', auth='public', csrf=False, website=True)
+    def list(self, **kw):
+        return request.render('academy.city_details', {
+            'objects': request.env['res.partner'].search([]),
+        })
+    # Ended by Appscomp
+
+    @http.route('/academy/<int:id>/', csrf=False, auth='public', website=True)
+    def academybyid(self, id):
+        print('idssssssssssssssss', id)
+        return request.render('academy.template_id', {'id': id
+                                                      })
+
+    @http.route('/academy/<model("academy.academy"):obj>', auth='public', website=True)
+    def academy_list(self, obj):
+        print('BOjectttttttttttt', obj)
+        return request.render('academy.object', {
+            'objects': obj,
+        })
+
+    @http.route('/academy/academy/create', auth='public', website=True)
+    def academy_created(self, **kw):
+        AcademyObj = request.env['academy.academy']
+        vals = {
+            'name': 'Odoo Training-123',
+            'city': 'Coimbatore'
+        }
+        AcademyObj.sudo().create(vals)
+        return request.render('academy.academy_created', {})
+
+    # Method Used for RPC cALL
+    @http.route('/add/value', auth='public', website=True)
+    def add_value(self, **kw):
+        return request.render('academy.add_value', {})
+
+    # tHIS METHOD CALLED FROM JS AND VALUE RETURN TO JS
+    @http.route('/add/value/sum', type="json", auth='public')
+    def add_value_sum(self, **kw):
+        print('summmmmmmmmmmmmmmmmmmmmm', kw)
+        num1 = kw.get('num1')
+        num2 = kw.get('num2')
+        sum = num1 + num2
+        return {'sum': sum}
